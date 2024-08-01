df2e9b94f3be9aa462ba308cf361892ec3400f9e
dave8git <dave8git@gmail.com>
2024-08-01 14:53:49 +0200

    Fix categories are not working

--- src/js/script.js
+++ src/js/script.js
@@ -127,134 +127,165 @@ const select = {
 };
 
 
-  const app = {
-//   inputHide: function() {
-//     const thisApp = this; 
-//     console.log('input hide działa');
-//     const inputContainer = document.querySelector('.submitContainer');
-//     // console.log(inputContainer);
-//     // const findActionInput = document.querySelector('#findAction');
-//     // console.log('findActionInput', findActionInput);
+const app = {
+    //   inputHide: function() {
+    //     const thisApp = this; 
+    //     console.log('input hide działa');
+    //     const inputContainer = document.querySelector('.submitContainer');
+    //     // console.log(inputContainer);
+    //     // const findActionInput = document.querySelector('#findAction');
+    //     // console.log('findActionInput', findActionInput);
 
-//     document.querySelector('#findAction').addEventListener('click', function () {
-//         console.log('inputAction ruszyło');
-//         inputContainer.innerHTML = '';
-//     })
+    //     document.querySelector('#findAction').addEventListener('click', function () {
+    //         console.log('inputAction ruszyło');
+    //         inputContainer.innerHTML = '';
+    //     })
 
-//   },
-  initPages: function() {
-    const thisApp = this; 
-    
-    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    //   },
+    initPages: function () {
+        const thisApp = this;
 
-    thisApp.navLinks = document.querySelectorAll(select.nav.links);
-    const idFromHash = window.location.hash.replace('#/', '');
-    
-    thisApp.activatePage(idFromHash); 
+        thisApp.pages = document.querySelector(select.containerOf.pages).children;
 
-    let pageMatchingHash = thisApp.pages[0].id; 
+        thisApp.navLinks = document.querySelectorAll(select.nav.links);
+        const idFromHash = window.location.hash.replace('#/', '');
 
-    for(let page of thisApp.pages) {
-      if(page.id == idFromHash) {
-  // eslint-disable-next-line no-unused-vars
-        pageMatchingHash = page.id;
-        // console.log(pageMatchingHash);
-        break; // dzięki break nie zostaną wykonane dalsze pętle jeżeli warunek będzie prawidziwy. 
-      }
-    }
+        thisApp.activatePage(idFromHash);
 
-    for(let link of thisApp.navLinks) {
-      link.addEventListener('click', function(event){
-        const clickedElement = this;
-        event.preventDefault(); 
-        /* get page id from href attribute */
-        const id = clickedElement.getAttribute('href').replace('#', '');
+        let pageMatchingHash = thisApp.pages[0].id;
 
-        /* run thisApp.activePaget with that id */ 
-        thisApp.activatePage(id);
-
-        /* change URL hash */
-        window.location.hash = '#/' + id;
-  
-      });
-    }
-  },
-
-  activatePage: function(pageId) {
-    const thisApp = this; 
-    /* add class "active" to matching pages, remove from non-matching */
-    for(let page of thisApp.pages) {
-      page.classList.toggle(classNames.pages.active, page.id == pageId);
-    }
-    /* add class "active" to matching links, remove from non-matching */
- 
-    for(let link of thisApp.navLinks) {
-      link.classList.toggle(
-        classNames.nav.active, 
-        link.getAttribute('href') == '#' + pageId
-      );
-    }
-  },
-  initData: function() {
-    const thisApp = this; 
-    thisApp.data = {};
-
-    const url = settings.db.url + '/' + settings.db.songs;
-
-    fetch(url)
-        .then(function(rawResponse){
-            return rawResponse.json(); 
-        })
-        .then(function(parsedResponse){
-            // console.log('parsedResponse', parsedResponse);
-
-            /* save parsedResponse as thisApp.data.products */
-            thisApp.data.songs = parsedResponse; 
-            // console.log(thisApp.data.songs);
-            /* execute initMenu method */ 
-            thisApp.listCategories(thisApp.data.songs);
-            thisApp.initAudio();
-        });
-
-    console.log('thisApp.data', JSON.stringify(thisApp.data));
-
-  },
-  listCategories: function(data) {
-    const categories = [];
-    for(let category of data) {
-        for(let singleCategory of category.categories) {
-            if(!categories.includes(singleCategory)) {
-                categories.push(singleCategory);
+        for (let page of thisApp.pages) {
+            if (page.id == idFromHash) {
+                // eslint-disable-next-line no-unused-vars
+                pageMatchingHash = page.id;
+                // console.log(pageMatchingHash);
+                break; // dzięki break nie zostaną wykonane dalsze pętle jeżeli warunek będzie prawidziwy. 
             }
         }
-    }
 
-    const categoriesContainer = document.querySelector(select.containerOf.categories);
-    let links = '';
+        for (let link of thisApp.navLinks) {
+            link.addEventListener('click', function (event) {
+                const clickedElement = this;
+                event.preventDefault();
+                /* get page id from href attribute */
+                const id = clickedElement.getAttribute('href').replace('#', '');
 
-    for (let category of categories) {
-        links += `<a href="#${category}">${category}</a>`
-    }
-    console.log('categories', categories);
-    categoriesContainer.innerHTML = links;
-  }, 
-  initAudio: function()  {
-    const thisApp = this; 
+                /* run thisApp.activePaget with that id */
+                thisApp.activatePage(id);
 
-    for (let song in thisApp.data.songs) {
-        //new AudioContainer(); 
-        new AudioContainer(thisApp.data.songs[song]);
-    }
-  },
+                /* change URL hash */
+                window.location.hash = '#/' + id;
 
-  init: function () {
-    const thisApp = this;
-    console.log('*** App starting ***');
-    thisApp.initPages();
-    thisApp.initData();
-    //thisApp.inputHide();
+            });
+        }
+    },
 
-  },
+    activatePage: function (pageId) {
+        const thisApp = this;
+        /* add class "active" to matching pages, remove from non-matching */
+        for (let page of thisApp.pages) {
+            page.classList.toggle(classNames.pages.active, page.id == pageId);
+        }
+        /* add class "active" to matching links, remove from non-matching */
+
+        for (let link of thisApp.navLinks) {
+            link.classList.toggle(
+                classNames.nav.active,
+                link.getAttribute('href') == '#' + pageId
+            );
+        }
+    },
+    initData: function () {
+        const thisApp = this;
+        thisApp.data = {};
+
+        const url = settings.db.url + '/' + settings.db.songs;
+
+        fetch(url)
+            .then(function (rawResponse) {
+                return rawResponse.json();
+            })
+            .then(function (parsedResponse) {
+                // console.log('parsedResponse', parsedResponse);
+
+                /* save parsedResponse as thisApp.data.products */
+                thisApp.data.songs = parsedResponse;
+                // console.log(thisApp.data.songs);
+                /* execute initMenu method */
+                thisApp.initData();
+                thisApp.initCategories(); 
+            });
+
+        console.log('thisApp.data', JSON.stringify(thisApp.data));
+
+    },
+    listCategories: function (data) {
+        const thisApp = this;
+        const categories = [];
+        for (let category of data) {
+            for (let singleCategory of category.categories) {
+                if (!categories.includes(singleCategory)) {
+                    categories.push(singleCategory);
+                }
+            }
+        }
+
+        const categoriesContainer = document.querySelector(select.containerOf.categories);
+        let links = '';
+
+        for (let category of categories) {
+            links += `<a href="#category-${category}">${category}</a>`
+        }
+        console.log('categories', categories);
+        categoriesContainer.innerHTML = links;
+
+        thisApp.initCategories();
+    },
+
+    initCategories: function () {
+        const thisApp = this;
+
+        const allCategories = document.querySelectorAll('a[href^="#category-"]');
+
+        for (let category of allCategories) {
+            category.addEventListener('click', thisApp.categoryListener);
+        }
+    },
+
+
+    generateDataByCategory: function () {
+        const thisApp = this;
+
+        console.log('thisApp', thisApp);
+    },
+
+    categoryListener: function () {
+        const thisApp = this;
+        //console.log('event.target', event.target);
+        const href = event.target.getAttribute('href');
+        console.log('href', href);
+        const categoryName = href.split('-')[1];
+        thisApp.generateDataByCategory(categoryName);
+    },
+
+    initAudio: function () {
+        const thisApp = this;
+
+        for (let song in thisApp.data.songs) {
+            //new AudioContainer(); 
+            new AudioContainer(thisApp.data.songs[song]);
+        }
+    },
+
+    init: function () {
+        const thisApp = this;
+        console.log('*** App starting ***');
+        thisApp.initPages();
+
+
+        //thisApp.inputHide();
+
+    },
 };
 
 app.init();
